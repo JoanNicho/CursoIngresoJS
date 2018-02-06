fd78fc118bb696a3e21d5b2579d8f384d76c9e5f
JoanNicho <joan_nicho@hotmail.com>
2018-02-06 12:32:35 -0300

    .

--- 9-Parciales/recuperatorio/dos.js
+++ 9-Parciales/recuperatorio/dos.js
@@ -1,4 +1,10 @@
 function Mostrar()
 {
-  
+  var importe;
+  var impdes
+  var porcentaje
+  importe=prompt("importe");
+  porcentaje=(importe*25)/100; 
+  impdes=importe-porcentaje;
+  document.getElementById("importeFinal").value=impdes;
 }
--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,23 @@
 function Mostrar()
 {
+var precio1;
+var precio2;
+var precio3;
+var suma;
+var promedio;
+precio1=document.getElementById("precioUno").value;
+precio2=document.getElementById("precioDos").value;
+precio3=document.getElementById("precioTres").value;
+suma=precio1+precio2+precio3;
+promedio=suma/3;
+alert(promedio);
+
+
+
+
+
+
+
+
 
 }
