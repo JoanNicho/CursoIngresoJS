4515fbefe127d50c8ce85b0fcbfe3bfb2ba94471
JoanNicho <joan_nicho@hotmail.com>
2018-02-06 11:39:25 -0300

    .

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,10 +4,11 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
-
-
-
-
-
-	
+var sueldo;
+var resultado;
+var total;
+sueldo=document.getElementById("sueldo").value;
+document.getElementById("resultado").value=resultado;
+total=sueldo.value*1.21;
+resultado(total);
 }
--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,8 @@
 function Mostrar()
 {
-  
+  var importe;
+  var importeiva;
+importe=prompt("importe");
+importeiva=importe*1.21;
+document.getElementById("importeFinal").value=importeiva;
 }
