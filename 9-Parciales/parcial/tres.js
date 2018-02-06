d54f3ebe43f370f355e01abfe15042bc73ab980e
JoanNicho <joan_nicho@hotmail.com>
2018-02-06 12:01:38 -0300

    .

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,10 @@
 function Mostrar()
 {
-
+var largo;
+var ancho;
+var perimetro;
+largo=document.getElementById("alrgo").value;
+ancho=document.getElementById("ancho").value;
+perimetro=(largo*2+ancho*2)*3;
+alert(perimetro);
 }
