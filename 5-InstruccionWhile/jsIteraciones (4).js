6039ecc0385a38dfc3241126a93cdf66c6d3f061
JoanNicho <joan_nicho@hotmail.com>
2018-02-19 12:14:37 -0300

    .

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -4,7 +4,7 @@ function Mostrar()
 	var numero = prompt("ingrese un número entre 0 y 10.");
 while(numero<0 || numero>9)
 {
-	
+	clave=prompt("ingrese un número entre 0 y 10.");
 }
 
 }//FIN DE LA FUNCIÓN
