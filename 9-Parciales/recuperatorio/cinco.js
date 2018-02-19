955c1b0c24cab887a8903850ba780346f94e0fef
JoanNicho <joan_nicho>
2018-02-19 10:29:19 -0300

    .

--- 9-Parciales/recuperatorio/cinco.js
+++ 9-Parciales/recuperatorio/cinco.js
@@ -1,4 +1,13 @@
 function Mostrar()
 {
-
+var mes=prompt("Elija un mes del año");
+switch(mes)
+{
+    case "Enero": alert("Comienza el año")
+    break;
+    case "Dicimebre": alert("Se vienen las fiestas")
+    break;
+    default: alert("No es Enero,ni Diciembre")
+    break;
+}
 }
