95a6123101482cfa18abe79fc69686bd32c3ff76
JoanNicho <joan_nicho@hotmail.com>
2018-02-20 11:43:59 -0300

    .

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,8 +3,13 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-
-
+	var num=prompt("Ingrese 5 números.");
+	while(num)
+	{
+		num=parseInt(num);
+		contador++;
+		acumulador+=num;
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,13 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var num
 	var respuesta='si';
-
+while(respuesta="si")
+{
+	num=prompt();
+	num=parseInt(num);
+}
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
