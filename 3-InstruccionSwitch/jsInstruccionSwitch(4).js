c5e8b698ff3d1558d26a6bd7e8a6745fb4e2c00a
JoanNicho <joan_nicho@hotmail.com>
2018-02-20 12:03:29 -0300

    .

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -2,11 +2,28 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño)
+{
+    case "Febrero":alert("Este mes tiene 28 dias")
+    break;
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":alert("Este mes tiene 30 días")
+   break;
+    case "Enero":
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Diciembre": alert("Este mes tiene 31 días")
+    break;
 
-//alert (mesDelAño);
+
 	
 	
 
 
-
+}
 }//FIN DE LA FUNCIÓN
