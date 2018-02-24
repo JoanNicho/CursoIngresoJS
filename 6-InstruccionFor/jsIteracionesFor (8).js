62133bbb93ac54ef7a3fa051777dc4b7207846ff
JoanNIcho <joan_nicho@hotmail.com>
2018-02-24 12:52:05 -0300

    .

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,9 +1,15 @@
 function Mostrar()
 {
+var cont=0;
+var rta="es primo";
 var num=prompt("");
-for(var i;;){
-    
+for(var i=2;i<num;i++){
+    if(num%i==0){
+        rta="no es primo";
+break;
 }
 
+}
+alert(rta);
 
 }//FIN DE LA FUNCIÓN
