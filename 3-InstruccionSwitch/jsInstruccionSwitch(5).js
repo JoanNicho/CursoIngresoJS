b741b61e2cf2f959ac06d0ff58da820e4320e471
JoanNIcho <joan_nicho@hotmail.com>
2018-02-24 09:36:41 -0300

    .

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -10,5 +10,15 @@ switch(laHora)
     case "10":
     case "11": alert("Es de mañana")
     break;
+    case "12":
+    case "13":
+    case "14":
+    case "15":
+    case "16":
+    case "17":
+    case "18":
+    case "19": alert("Es de tarde")
+    break;
+    case "20"
 }
 }//FIN DE LA FUNCIÓN
