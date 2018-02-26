ea9ea1161d9ca26217c72033060b734f3cf9d322
JoanNicho <joan_nicho@hotmail.com>
2018-02-26 11:30:20 -0300

    .

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -16,17 +16,70 @@ var sueldo=document.getElementById("Sueldo").value;
 var legajo=document.getElementById("Legajo").value;
 var nacionalidad=document.getElementById("Nacionalidad").value;
 
-edad=prompt("ingrese edad");
+edad=prompt("ingrese edad entre 18 y 90 años");
 while(edad<18 || edad>90 || isNaN(edad))
 {
-edad=prompt("ingrese edad");
+edad=prompt("ingrese edad entre 18 y 90 años");
 }
 document.getElementById("Edad").value=edad;
 
-sexo=prompt("ingrese sexo");
-while(sexo!="f" && sexo!="m")
+sexo=prompt("ingrese sexo F o M");
+while(sexo!="F" && sexo!="M")
 {
-sexo=prompt("ingrese sexo");
+sexo=prompt("ingrese sexo F o M");
 }
 document.getElementById("Sexo").value=sexo;
+
+ecivil=prompt("ingrese 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos")
+switch(ecivil){
+    case "1":document.getElementById("EstadoCivil").value="Soletro"
+    break;
+    case "2":document.getElementById("EstadoCivil").value="Casado"
+    break;
+    case "3":document.getElementById("EstadoCivil").value="Divociados"
+    break;
+    case "4":document.getElementById("EstadoCivil").value="Viudos"
+    break;
+}
+
+sueldo=prompt("ingrese sueldo no menor a 8000");
+while(sueldo<8000){
+sueldo=prompt("ingrese sueldo");
+}
+document.getElementById("Sueldo").value=sueldo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
