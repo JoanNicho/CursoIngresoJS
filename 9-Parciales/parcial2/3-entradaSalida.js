2df7d94eb03e39b169093a302e449ca8f0eb72c5
JoanNicho <joan_nicho@hotmail.com>
2018-02-27 10:08:09 -0300

    .

--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,9 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
-{
-	alert("Funciona 3-EntradaSalida");
+{var largo=document.getElementById("largo").value*2;
+	var ancho=document.getElementById("ancho").value*2;
+	var perimetro=(parseInt(largo)+parseInt(ancho))*6;
+	alert("la cantidad de alambrado que se necesita es de:"+perimetro);
 	
 }
 
