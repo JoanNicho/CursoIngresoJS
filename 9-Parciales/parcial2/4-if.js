df4af3eab9c904baa0afe82ef0142a111ca83c10
JoanNicho <joan_nicho@hotmail.com>
2018-02-27 10:24:05 -0300

    .

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,39 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
-	
+var	num1=prompt("Ingrese el primer número");
+var num2=prompt("Ingrese el segundo número");
+var resultado
+if(num1===num2){
+resultado=parseInt(num1)*parseInt(num2);
+document.write("El resultado es:"+resultado);
+}
+else{
+	if(num1>num2){
+resultado=parseInt(num1)-parseInt(num2);
+document.write("El resultado es:"+resultado);
+	}
+	else{
+		if(num1<num2){
+			resultado=parseInt(num1)+parseInt(num2);
+			document.write("El resultado es:"+resultado);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
